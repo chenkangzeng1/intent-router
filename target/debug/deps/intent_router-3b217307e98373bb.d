7025aedf803ae41dc4fa755147e1a93c48332dfb
Kangzeng Chen <chenkangzeng@163.com>
2026-06-07 13:17:25 +0800

    Add .gitignore

--- target/debug/deps/intent_router-3b217307e98373bb.d
+++
@@ -1,8 +0,0 @@
-/Users/kangzengchen/source/buka/buka-works/intent-router/target/debug/deps/intent_router-3b217307e98373bb.d: src/lib.rs src/router.rs
-
-/Users/kangzengchen/source/buka/buka-works/intent-router/target/debug/deps/libintent_router-3b217307e98373bb.rlib: src/lib.rs src/router.rs
-
-/Users/kangzengchen/source/buka/buka-works/intent-router/target/debug/deps/libintent_router-3b217307e98373bb.rmeta: src/lib.rs src/router.rs
-
-src/lib.rs:
-src/router.rs:
--- target/debug/deps/intent_router-c2b3ca124bb2f95a.d
+++
@@ -1,6 +0,0 @@
-/Users/kangzengchen/source/buka/buka-works/intent-router/target/debug/deps/intent_router-c2b3ca124bb2f95a.d: src/lib.rs src/router.rs
-
-/Users/kangzengchen/source/buka/buka-works/intent-router/target/debug/deps/intent_router-c2b3ca124bb2f95a: src/lib.rs src/router.rs
-
-src/lib.rs:
-src/router.rs:
